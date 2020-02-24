e9bf45af2e21a1634e7346cb85f1e978ea3cc9cc
songjiawei <songjiawei@cmss.chinamobile.com>
2020-02-24 16:56:16 +0800

    webpack

--- webpack/my-webpack/bin/main-webpack.js
+++ webpack/my-webpack/bin/main-webpack.js
@@ -1,2 +1,8 @@
 #! /usr/bin/env node
-console.log(123)
+const path = require("path");
+
+// 1.读取需要打包项目的配置文件
+let config = require(path.resolve("webpack.config.js"));
+// 2.通过面向对象的方式推进项目
+let Compiler = require("../lib/Bundle.js");
+new Compiler(config).start();
---
+++ webpack/my-webpack/lib/Bundle.js
@@ -0,0 +1,13 @@
+class Compiler {
+  constructor(config) {
+    this.config = config;
+  }
+  depAnalyes(){
+
+  }
+  // 项目开始打包
+  start() {
+    // 依赖分析
+    this.depAnalyes();
+  }
+}
