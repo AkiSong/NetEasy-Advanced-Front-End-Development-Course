b072faa74d4b27bc3307c1c06ceee9cd62567111
songjiawei <songjiawei@cmss.chinamobile.com>
2020-03-18 17:05:20 +0800

    10th

--- 10thPower/10thProject/app.js
+++ 10thPower/10thProject/app.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+const path = require('path')
 
 const app = express()
 
+nunjucks.configure(path.join(__dirname, './view/'), {
+    autoescape: true,
+    express: app,
+    watch: true //禁用模板文件缓存
+});
+app.use('/public/', express.static(path.join(__dirname, './public/')))
 app.get('/', (req, res, next) => {
-  res.status(200).send('Hello Express')
+  res.render('index.html')
+})
+app.get('/people/home', (req, res, next) => {
+  res.render('people-home.html')
 })
 
 app.listen(3000, ()=>{
