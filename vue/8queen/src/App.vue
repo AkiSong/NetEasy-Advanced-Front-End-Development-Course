dd19ddc236ce55fff48ce7042f3cf0d2614dfe44
songjiawei <songjiawei@cmss.chinamobile.com>
2020-04-07 17:55:48 +0800

    vue

---
+++ vue/8queen/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <eight-queen></eight-queen>
+  </div>
+</template>
+
+<script>
+import EightQueen from "./components/EightQueen.vue";
+export default {
+  name: "App",
+  components: {
+    EightQueen,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/8queen/src/components/EightQueen.vue
@@ -0,0 +1,33 @@
+<script>
+const grids = new Array(8)
+  .fill(1)
+  .map((_, r) =>
+    new Array(8)
+      .fill(1)
+      .map((_, c) => ({ key: `key-${r * 8}-${c}`, ok: false }))
+  );
+export default {
+  data() {
+    return {
+      grids,
+    };
+  },
+  render() {
+    return (
+      <div class="grid">
+        {this.grids.map((row, index) => {
+          return (
+            <div class="row" key={index}>
+              {row.map((cell, i) => {
+                return <div class="cell" key={i}></div>;
+              })}
+            </div>
+          );
+        })}
+      </div>
+    );
+  },
+};
+</script>
+
+<style></style>
